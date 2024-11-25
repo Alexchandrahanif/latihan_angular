63475df3e88ee4b46f19dd2c20a8421bc0d257d7
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-11-26 06:15:46 +0700

    feat : add routing and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/Home/home.page';
+import { NotFoundComponent } from './pages/NotFound/notfound.page';
+import { LoginComponent } from './pages/Login/login.page';
+import { RegisterComponent } from './pages/Register/register.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/pages/NotFound/notfound.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './notfound.page.html',
+})
+export class NotFoundComponent {}
